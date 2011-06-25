afa6d2567536191d9c45756b778fd3cd5b4c926a
Joe <joe@osdf.com>
2011-06-25 16:02:41 -0700

    Admin tweaks, TinyMCE optional init

--- quickpages/admin.py
+++ quickpages/admin.py
@@ -7,20 +7,20 @@ from quickpages.models import QuickPage
 
 
 class QuickPageAdmin (admin.ModelAdmin):
-  fieldsets  = (#(None, {'fields': ('site', 'name', 'title', 'typ')}),
-                (None, {'fields': (('name', 'title', 'published'),)}),
-                ('Extras', {'classes': ['collapse'], 'fields': (('javascript','css'), 'heading')}),
-                #('Extras', {'classes': ['collapse'], 'fields': (('javascript','css'), ('heading', 'headingtag'))}),
-                ('Meta', {'classes': ['collapse'], 'fields': ('description','keywords')}),
-                ('Comments', {'classes': ['collapse','noedit'], 'fields': ('comments',)}),
-                (None, {'classes': ['edit'], 'fields': ('content',)}),  # , 'wide' is NFG
-               )
+    fieldsets  = (
+                  (None, {'fields': (('name', 'title', 'published'),)}),
+                  ('Extras', {'classes': ['collapse'], 'fields': (('javascript','css'), 'heading')}),
+                  ('Meta', {'classes': ['collapse'], 'fields': ('description','keywords')}),
+                  ('Comments', {'classes': ['collapse','noedit'], 'fields': ('comments',)}),
+                  (None, {'classes': ['edit'], 'fields': ('content',)}),  # , 'wide' is NFG
+                 )
 
-  list_display = ('name', 'title', 'published', 'comments') 
-  save_as = True
+    list_filter = ('published', 'created', 'updated')
+    list_display = ('name', 'title', 'published', 'comments', 'created', 'updated') 
+    save_as = True
 
-  class Media:
-    js = ['/tiny_mce/tiny_mce.js', '/js/textareas.js'] # JJW 7/4/07, 3/20/08
+    class Media:
+        js = ('/media/tinymce/tiny_mce.js','/media/tinymce/mce_setup.js')
 
 
 admin.site.register (QuickPage, QuickPageAdmin)
