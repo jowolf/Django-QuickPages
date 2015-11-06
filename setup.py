7641c2f8feaf67c710ba9390188e732a091b2a12
Joe <joe@osdf.com>
2015-11-06 12:34:24 -0800

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup (
+    name         = 'Django-QuickPages',
+    packages     = ['quickpages'],
+    version      = '1.0.1',
+    description  = 'Simliar to django-flatpages, but better & more flexible - also does snippets.',
+    author       = 'Joseph Wolff',
+    author_email = 'joe@eracks.com',
+    url          = 'https://github.com/jowolf/Django-QuickPages',
+    download_url = 'https://github.com/jowolf/Django-QuickPages/archive/master.zip',
+    license      = "BSD",
+)
